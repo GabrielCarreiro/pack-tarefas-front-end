8edf57945cdb24779452b5b1fe8a1d6a2d94a528
Gabriel Carreiro dos Santos <63800998+GabrielCarreiro@users.noreply.github.com>
2021-01-18 13:31:10 -0300

    config heroku

---
+++ src/components/Drawer.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="page-container">
+        <md-app>
+            <md-app-toolbar class="md-primary" md-elevation="2">
+                <md-button
+                    class="md-icon-button"
+                    @click="toggleMenu"
+                    v-if="!menuVisible">
+                    <md-icon>menu</md-icon>
+                </md-button>
+                <span class="md-title"> Pack Tarefas</span>
+            </md-app-toolbar>
+            <md-app-drawer :md-active.sync="menuVisible" md-persistent="mini">
+                <md-toolbar class="md-transparent" md-elevation="0">
+                    <md-icon>menu_open</md-icon>
+                    <span class="md-headline">Menu</span>
+ 
+                    <div class="md-toolbar-section-end">
+                        <md-button class="md-icon-button md-dense" @click="toggleMenu">
+                            <md-icon>keyboard_arrow_left</md-icon>
+                        </md-button>
+                     </div>
+                </md-toolbar>
+                <md-list>
+                    <router-link to="/">
+                        <md-list-item class="link">      
+                            <md-icon>wysiwyg</md-icon>
+                            <span class="md-list-item-text" >Todas Tarefas</span>
+                        </md-list-item>
+                    </router-link>
+                    <router-link to="/tarefas">
+                        <md-list-item class="link">
+                            <md-icon>how_to_reg</md-icon>
+                            <span class="md-list-item-text">Minhas Tarefas</span>
+                        </md-list-item>
+                    </router-link>
+                    <md-list-item class="link">
+                        <md-icon>settings</md-icon>
+                        <span class="md-list-item-text">Configuração</span>
+                    </md-list-item>
+                    <md-list-item class="link">
+                        <md-icon>error</md-icon>
+                        <span class="md-list-item-text">Ajuda</span>
+                    </md-list-item>
+                    <md-list-item class="link">
+                        <md-icon>exit_to_app</md-icon>
+                        <span class="md-list-item-text">Sair</span>
+                    </md-list-item>
+                </md-list>
+            </md-app-drawer>
+            <md-app-content>
+                <router-view></router-view>
+            </md-app-content>
+        </md-app>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+
+  name: "PersistentMini",
+  data: () => ({
+    menuVisible: false,
+  }),
+  methods: {
+    toggleMenu() {
+      this.menuVisible = !this.menuVisible;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.md-app {
+  height: 650px;
+  border: 1px solid rgba(#000, 0.12);
+  background-color: red;
+}
+
+// Demo purposes only
+.md-drawer {
+  width: 200px;
+  max-width: calc(100vw - 125px);
+}
+.link{
+    cursor: pointer;
+}
+
+</style>
---
+++ src/views/TasksUser.vue
@@ -0,0 +1,212 @@
+<template>
+  <div>
+    <md-progress-spinner v-if="loading" :md-diameter="200" :md-stroke="20" md-mode="indeterminate"></md-progress-spinner>
+    <md-table v-else v-model="tasks" md-card @md-selected="onSelect">
+      <md-table-toolbar>
+        <h1 class="md-display-1 title">Minhas Tarefas</h1>
+        <md-field md-clearable class="md-toolbar-section-end">
+        <md-input
+        placeholder="Procurar por tarefa"
+        v-model="search"
+        @input="searchOnTable"/>
+        </md-field>
+      </md-table-toolbar>
+
+      <md-table-toolbar slot="md-table-alternate-header" id="tr" slot-scope="{ count }">
+        <div class="md-toolbar-section-start">{{ getAlternateLabel(count) }}</div>
+
+        <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button" @click="DeleteAllTasks">
+                <md-icon>delete</md-icon>
+            </md-button>
+            <md-button class="md-icon-button" @click="UpdateAllTasksCompleted">
+                <md-icon>check_circle_outline</md-icon>
+            </md-button>
+            <md-button class="md-icon-button" @click="UpdateAllTasksPending">
+                <md-icon>panorama_fish_eye</md-icon>
+            </md-button>
+            <md-button class="md-icon-button" @click="UpdateAllTasksProgress">
+                <md-icon>running_with_errors</md-icon>
+            </md-button>
+        </div>
+      </md-table-toolbar>
+
+      <md-table-row id="rows" slot="md-table-row" slot-scope="{ item }" md-selectable="multiple">
+        <md-table-cell id="rows" md-label="Ocorrência" md-sort-by="ocorrencia" class="left">{{ item.task }}</md-table-cell>
+        <md-table-cell id="rows" md-label="Solicitante" md-sort-by="solicitante" class="left">{{ item.requester }}</md-table-cell>
+        <md-table-cell id="rows" md-label="Tipo" md-sort-by="Tipo" class="left">{{ item.type }}</md-table-cell>
+        <md-table-cell id="rows" md-label="Prazo" md-sort-by="prazo" class="left">{{ item.term }}</md-table-cell>
+        <md-table-cell id="rows" md-label="Status" md-sort-by="status" class="left">{{ item.status }}</md-table-cell>
+        <md-table-cell id="rows" md-label="Ações" md-sort-by="acoes" class="left">
+            <span v-if="item.status === 'Concluido' ">
+                <button class="btn-action" @click="UpdateTasksPending(item)">
+                    <md-icon>check_circle_outline</md-icon> 
+                </button>
+                <button class="btn-action" @click="DeleteTaks(item)">
+                    <md-icon>delete_forever</md-icon>
+                </button>
+            </span>
+            <span v-else-if="item.status === 'Em andamento' ">
+                <button class="btn-action" @click="UpdateTasksCompleted(item)">
+                    <md-icon>running_with_errors</md-icon> 
+                </button>
+            </span>
+            <span v-else>
+                <button class="btn-action"  @click="UpdateTasksProgress(item)">
+                    <md-icon>panorama_fish_eye</md-icon>
+                </button>
+            </span>
+        </md-table-cell> 
+      </md-table-row>
+    </md-table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const toLower = (text) => {
+    return text.toString().toLowerCase();
+};
+
+const searchByName = (items, term) => {
+    if (term) {
+        return this.tasks.filter((item) => toLower(item.task).includes(toLower(term)));
+    }
+  return items;
+};
+
+  export default {
+    name: 'TableMultiple',
+    data: () => ({
+      selected: [],
+      tasks: [],
+      loading : false
+    }),
+    methods: {
+        onSelect (items) {
+            this.selected = items
+        },
+         searchOnTable() {
+            this.searched = searchByName(this.tasks, this.search);
+        },
+        getAlternateLabel (count) {
+            let plural = ''
+
+            if (count > 1) {
+                plural = 's'
+            }
+            return `${count} user${plural} selected`
+        },
+        UpdateTasksCompleted(item){
+            const params = {
+                ...item,
+                status: "Concluido"
+            }
+            this.loading = true;
+            axios.put(`http://localhost:3333/tasks/${item.id}`, params);
+            this.UpdateTasks()
+        },
+        UpdateTasksPending(item){
+            const params = {
+                ...item,
+                status: "Pendente"
+            }
+            this.loading = true;
+            axios.put(`http://localhost:3333/tasks/${item.id}`, params);
+            this.UpdateTasks()
+        },
+         UpdateTasksProgress (item)  {
+            const params = {
+                ...item,
+                status: "Em andamento"
+            } 
+            this.loading = true;
+            axios.put(`http://localhost:3333/tasks/${item.id}`, params);
+            this.UpdateTasks()
+        }, 
+        UpdateAllTasksPending (){
+            this.loading = true;
+          
+            axios.put(`http://localhost:3333/tasks/all/pending`, this.selected).then(()=>{
+                setTimeout(() => {
+                    this.UpdateTasks()
+                }, 2000);
+            })
+            
+        },  UpdateAllTasksProgress (){
+            this.loading = true;
+          
+            axios.put(`http://localhost:3333/tasks/all/progress`, this.selected).then(()=>{
+                setTimeout(() => {
+                    this.UpdateTasks()
+                }, 2000);
+            })
+            
+        },
+        UpdateAllTasksCompleted (){
+            this.loading = true;
+          
+            axios.put(`http://localhost:3333/tasks/all/completed`, this.selected).then(()=>{
+                setTimeout(() => {
+                    this.UpdateTasks()
+                }, 2000);
+            })
+            
+        },
+        DeleteTaks(item){
+            this.loading = true;
+            axios.delete(`http://localhost:3333/tasks/delete/${item.id}`);
+            this.UpdateTasks()
+        },
+        DeleteAllTasks(){
+            this.loading = true;
+            axios.post(`http://localhost:3333/tasks/delete/all`, this.selected).then(() =>{
+                setTimeout(() => {
+                    this.UpdateTasks()
+                }, 2000);
+            })
+        },
+        UpdateTasks(){
+           setTimeout(() => {
+                axios.get('http://localhost:3333/tasks/1').then(res=>{
+                    this.loading = false;
+                    this.tasks = res.data;
+                })
+           }, 1000);
+        } 
+    },
+    created: function (){
+        axios.get('http://localhost:3333/tasks/1').then(res=>{
+            this.tasks = res.data;
+            console.log(res.data)
+        }).catch(function(error){
+            console.log(error)
+        })  
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+    .md-table + .md-table {
+        margin-top: 90px
+    }
+    .left{
+        text-align: left;
+    }
+    #rows{
+       padding: 15px 0;
+    }
+    #tr{
+        background-color:#4d8ffe;
+    }
+    .title{
+        margin-right: 180px;
+    }.btn-action{
+        border: 0;
+        background-color: transparent;
+        cursor: pointer;
+        padding: 0;
+        margin: 0 2px;
+    }
+</style>
